d51a7e8e5cebddffdd30e533474710874994fbaf
Joachim Kiedrowski <joachim.kie@googlemail.com>
2020-03-22 13:05:50 +0100

    add about-us and reset-password page

---
+++ pages/about-us.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .about-us about us
+</template>
+
+<script>
+export default {
+  layout: 'default'
+}
+</script>
+
+<style lang="scss">
+.about-us {
+  background: red;
+}
+</style>
+
+
---
+++ pages/reset-password.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .reset-password
+    .form-wrapper
+      h1.title Passwort vergessen
+      form
+        input#email(type='text' name="email" placeholder="Email")
+        input.button(type='submit' value='Passwort anfordern')
+      div.link--small
+        span Brauchst du einen Account?
+          nuxt-link.primary-color(to='/register')  Registrieren
+      div.link--small
+        span Zurück zum
+          nuxt-link.primary-color(to='/login')  Login
+</template>
+
+<script>
+export default {
+  layout: 'default'
+}
+</script>
+
+<style lang="scss">
+.reset-password {
+  .button {
+    width: unset;
+    margin-bottom: 16px;
+  }
+}
+</style>
