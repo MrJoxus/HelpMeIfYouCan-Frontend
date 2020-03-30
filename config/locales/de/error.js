3b280bb040b02086c9eab2590c2123020a14ad17
Joachim Kiedrowski <joachim.kie@googlemail.com>
2020-03-30 15:15:07 +0200

    add erorr messages object

---
+++ config/locales/de/error.js
@@ -0,0 +1,11 @@
+export default {
+  addressSearch: {
+    notFound: 'Adresse konnte nicht gefunden werden.',
+    postalCodeMissing: 'Postleitzahl fehlt.',
+    streenNumberMissing: 'Hausnummer fehlt.',
+    areaMissing: 'Ort fehlt.'
+  },
+  registration: {
+    emailAlreadyTaken: 'Diese Email ist bereits vergeben.'
+  }
+}
